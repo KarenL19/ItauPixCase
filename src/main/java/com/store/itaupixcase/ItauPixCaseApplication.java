b68db7e724c9273553e555a44738129d0a1dac9f
KarenL19 <karen-_-19@outlook.com>
2025-06-08 11:37:46 -0300

    Initializer project

---
+++ src/main/java/com/store/itaupixcase/ItauPixCaseApplication.java
@@ -0,0 +1,13 @@
+package com.store.itaupixcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauPixCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItauPixCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/store/itaupixcase/ItauPixCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.itaupixcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauPixCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
